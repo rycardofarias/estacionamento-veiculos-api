804be9d99c4b89c4bacd21c0899cd1076f75ec50
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-25 19:06:06 -0300

    Config Timezone and Locale

---
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/config/SpringTimeZoneConfig.java
@@ -0,0 +1,15 @@
+package com.rycardofarias.estacionamentoveiculoapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimeZoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
