4ff6f7cfa9d35e87bac5c76b3df10db240b5bc59
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-25 17:49:12 -0300

    Starting project

---
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/EstacionamentoVeiculoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rycardofarias.estacionamentoveiculoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoVeiculoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoVeiculoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rycardofarias/estacionamentoveiculoapi/EstacionamentoVeiculoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rycardofarias.estacionamentoveiculoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoVeiculoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
