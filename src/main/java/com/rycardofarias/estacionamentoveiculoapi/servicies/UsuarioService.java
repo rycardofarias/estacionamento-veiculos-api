dc5c9676a0b80508a48d658d3eedecb6b10cfe71
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-26 00:05:36 -0300

    User get by id

--- src/main/java/com/rycardofarias/estacionamentoveiculoapi/servicies/UsuarioService.java
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/servicies/UsuarioService.java
@@ -1,12 +1,26 @@
 package com.rycardofarias.estacionamentoveiculoapi.servicies;
 
+import com.rycardofarias.estacionamentoveiculoapi.entities.Usuario;
 import com.rycardofarias.estacionamentoveiculoapi.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salver(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+    }
 }
--- src/main/java/com/rycardofarias/estacionamentoveiculoapi/web/controllers/UsuarioController.java
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/web/controllers/UsuarioController.java
@@ -1,9 +1,11 @@
 package com.rycardofarias.estacionamentoveiculoapi.web.controllers;
 
+import com.rycardofarias.estacionamentoveiculoapi.entities.Usuario;
 import com.rycardofarias.estacionamentoveiculoapi.servicies.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -11,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    private ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salver(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> create(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
 }
