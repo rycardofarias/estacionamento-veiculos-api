9599abd2a44b41b002e35a65c4708ff7f575bc6e
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-27 00:04:52 -0300

    Configuring the test layer

---
+++ src/test/java/com/rycardofarias/estacionamentoveiculoapi/UsuarioIT.java
@@ -0,0 +1,15 @@
+package com.rycardofarias.estacionamentoveiculoapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "/sql/usuarios/usuarios-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
+public class UsuarioIT {
+
+    @Autowired
+    WebTestClient testClient;
+}
