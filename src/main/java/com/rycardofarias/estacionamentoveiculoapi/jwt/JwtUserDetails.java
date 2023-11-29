fcbad46d0e6973023bdba9c54aa7ec7bf5921f9c
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-11-29 00:40:49 -0300

    Created JwtUserDetails

---
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/jwt/JwtUserDetails.java
@@ -0,0 +1,26 @@
+package com.rycardofarias.estacionamentoveiculoapi.jwt;
+
+import com.rycardofarias.estacionamentoveiculoapi.entities.Usuario;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(
+                usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+
+
+}
