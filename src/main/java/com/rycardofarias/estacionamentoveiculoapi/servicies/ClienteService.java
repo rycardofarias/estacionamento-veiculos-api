8aaf2a6134a271103c37554e2b3b61797f3a0ef0
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-12-06 22:43:39 -0300

    Creating documentation for the clientController and creating the buscaPorId method

--- src/main/java/com/rycardofarias/estacionamentoveiculoapi/servicies/ClienteService.java
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/servicies/ClienteService.java
@@ -2,6 +2,7 @@ package com.rycardofarias.estacionamentoveiculoapi.servicies;
 
 import com.rycardofarias.estacionamentoveiculoapi.entities.Cliente;
 import com.rycardofarias.estacionamentoveiculoapi.exceptions.CpfUniqueViolationException;
+import com.rycardofarias.estacionamentoveiculoapi.exceptions.EntityNotFoundException;
 import com.rycardofarias.estacionamentoveiculoapi.repositories.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -22,5 +23,11 @@ public class ClienteService {
             throw new CpfUniqueViolationException(String.format("CPF '%s' não pode ser cadastrado, já existe no " +
                     "sistema", cliente.getCpf()));
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cliente id=%s não encontrado no sistema", id)));
     }
 }
--- src/main/java/com/rycardofarias/estacionamentoveiculoapi/web/controllers/ClienteController.java
+++ src/main/java/com/rycardofarias/estacionamentoveiculoapi/web/controllers/ClienteController.java
@@ -2,21 +2,24 @@ package com.rycardofarias.estacionamentoveiculoapi.web.controllers;
 
 import com.rycardofarias.estacionamentoveiculoapi.dtos.ClienteCreateDto;
 import com.rycardofarias.estacionamentoveiculoapi.dtos.ClienteResponseDto;
+import com.rycardofarias.estacionamentoveiculoapi.dtos.UsuarioCreateDto;
 import com.rycardofarias.estacionamentoveiculoapi.dtos.mappers.ClienteMapper;
 import com.rycardofarias.estacionamentoveiculoapi.entities.Cliente;
 import com.rycardofarias.estacionamentoveiculoapi.jwt.JwtUserDetails;
 import com.rycardofarias.estacionamentoveiculoapi.servicies.ClienteService;
 import com.rycardofarias.estacionamentoveiculoapi.servicies.UsuarioService;
+import com.rycardofarias.estacionamentoveiculoapi.web.exceptions.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,8 +29,26 @@ public class ClienteController {
     private final ClienteService clienteService;
     private final UsuarioService usuarioService;
 
+    @Operation(summary = "Criar um novo cliente", description = "Recurso para criar um novo cliente Requisção exige " +
+            "o uso de um bearer token. Acesso restrito a usuários com perfil CLIENTE.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    UsuarioCreateDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já possui cadastro no sistema",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada invalidos",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de ADMIN",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    ErrorMessage.class)))
+
+            }
+    )
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_CLIENTE')")
+    @PreAuthorize("hasRole('CLIENTE')")
     public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto clienteCreateDto,
                                                     @AuthenticationPrincipal JwtUserDetails userDetails) {
 
@@ -35,5 +56,27 @@ public class ClienteController {
         cliente.setUsuario(usuarioService.buscarPorId(userDetails.getId()));
         clienteService.salvar(cliente);
         return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
+    }
+
+    @Operation(summary = "Buscar um cliente", description = "Recurso para buscar um cliente pelo ID. Requisção exige " +
+            "o uso de um bearer token. Acesso restrito a usuários com perfil 'ADMIN'.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    UsuarioCreateDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Cliente não encontrado",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation =
+                                    ErrorMessage.class)))
+
+            }
+    )
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ClienteResponseDto> getById(@PathVariable Long id) {
+        Cliente cliente = clienteService.buscarPorId(id);
+        return ResponseEntity.ok(ClienteMapper.toDto(cliente));
     }
 }
